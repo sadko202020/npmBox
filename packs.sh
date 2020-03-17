d3ca61556c4af5b90bd3d41a36d2ee249e593988
sadko202020 <61873768+sadko202020@users.noreply.github.com>
2020-03-17 16:30:26 +0500

    Update packs sh

--- packs.sh
+++ packs.sh
@@ -1,2 +1,2 @@
-#yarn add webpack webpack-cli babel-core babel babel-loader babel-preset-react @babel/core @babel/preset-env @babel/preset-react react react-dom redux react-redux redux-thunk
-yarn
+yarn add webpack webpack-cli babel-loader @babel/core @babel/preset-env @babel/preset-react react react-dom redux react-redux redux-thunk
+#yarn
